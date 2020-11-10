b274144b7c7bb69365318e1431d722b70fa85299
Franco Ortega <francopdx@gmail.com>
2020-11-09 17:45:12 -0800

    clean up eslint errors in utils.js

--- utils.js
+++ utils.js
@@ -1,35 +1,12 @@
 function geoMunge(geoData) {
-  
-    const firstItem = geoData[0];
-    return {
-      formatted_query: firstItem.display_name,
-      latitude: firstItem.lat,
-      longitude: firstItem.lon
-    };
-  
-    
+
+  const firstItem = geoData[0];
+  return {
+    formatted_query: firstItem.display_name,
+    latitude: firstItem.lat,
+    longitude: firstItem.lon
+  };
 }
-
-//   return {
-//     geoData.map((oneData) => {
-
-
-
-//     });
-//   }
-// }
-
-
-
-//   return {
-//     formatted_query: geoData.display_name,
-//     latitude: geoData.lat,
-//     longitude: geoData.lon,
-//     test: 'test string'
-//   };
-
-
-
 
 module.exports = {
   geoMunge
