6810c3f32b1b4b52bb4187c6751a6304ef797fdb
Franco Ortega <francopdx@gmail.com>
2020-11-11 08:22:37 -0800

    start lab 12 cos I do not know how to fix the lab 10 problems and it is too early to get help, so add todos.js

---
+++ data/todos.js
@@ -0,0 +1,17 @@
+module.exports = [
+  {
+    id: 1,
+    todo: 'make breakfast',
+    completed: false
+  },
+  {
+    id: 2,
+    todo: 'make lunch',
+    completed: false
+  },
+  {
+    id: 3,
+    todo: 'make dinner',
+    completed: false
+  }
+];
